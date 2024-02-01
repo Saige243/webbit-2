d3e762c4c0ff2a19356adeaf4c937ce85c865ced
Saige Cross <saigecrossdev@gmail.com>
2024-02-01 11:58:09 -0600

    added sub types

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -1,3 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {}
+export default class extends Controller {
+  static targets = ["button", "text", "url", "media"]
+  connect() {}
+  toggle(event) {
+    event.preventDefault()
+    if (event.target.dataset.tabId === "text") {
+      this.textTarget.classList.remove("hidden")
+      this.urlTarget.classList.add("hidden")
+      this.mediaTarget.classList.add("hidden")
+    } else if (event.target.dataset.tabId === "url") {
+      this.textTarget.classList.add("hidden")
+      this.urlTarget.classList.remove("hidden")
+      this.mediaTarget.classList.add("hidden")
+    } else if (event.target.dataset.tabId === "media") {
+      this.textTarget.classList.add("hidden")
+      this.urlTarget.classList.add("hidden")
+      this.mediaTarget.classList.remove("hidden")
+    } else {
+      this.textTarget.classList.add("hidden")
+      this.urlTarget.classList.add("hidden")
+      this.mediaTarget.classList.add("hidden")
+    }
+  }
+}
